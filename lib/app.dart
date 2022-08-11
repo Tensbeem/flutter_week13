ea982d9dd684668b9e4c4fb445795aeff45544c7
Tensbeam <tensrrt123@gmail.com>
2022-08-11 16:06:57 +0700

    update

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_week13/row_page.dart';
+import 'colum_page.dart';
+import 'card_page.dart';
+
+class  MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CardPage(),
+    );
+  }
+}
---
+++ lib/card_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class  CardPage extends StatelessWidget {
+  const CardPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Card page"),
+      ),
+      body: Center(
+        child: Card(
+          child: InkWell(
+            splashColor: Colors.redAccent.withAlpha((30)),
+            onTap: () {},
+            child: SizedBox(
+              width: 300,
+              height: 100,
+              child: Column(
+                children: [
+                  Icon(Icons.home),
+                  Text('MyHome')
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
