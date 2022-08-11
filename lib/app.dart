5551af89082a6c36e91dc6678a5f4ba716241f7e
Tensbeam <tensrrt123@gmail.com>
2022-08-11 16:59:16 +0700

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_week13/gridview_card_page.dart';
 import 'package:flutter_week13/row_page.dart';
 import 'colum_page.dart';
 import 'card_page.dart';
@@ -9,7 +10,7 @@ class  MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CardPage(),
+      home: GridViewPage(),
     );
   }
 }
--- lib/gridview_card_page.dart
+++ lib/gridview_card_page.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
 
-class  GridViewPage extends StatelessWidget {
+class Menu{
+  final String? title;
+  final IconData? icon;
+  final Color? bkColor;
+
+  Menu({this.title,this.icon,this.bkColor});
+}
+
+List<Menu> menus = [
+  Menu(title: "MENU-1",icon:Icons.person, bkColor: Colors.redAccent),
+  Menu(title: "MENU-2",icon:Icons.person_add, bkColor: Colors.blue),
+  Menu(title: "MENU-3",icon:Icons.person_off, bkColor: Colors.purple),
+];
+
+class GridViewPage extends StatelessWidget {
   const GridViewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: EdgeInsets.all(8),
+        child: GridView.count(
+            crossAxisCount: menus.length,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            // children: List.generate(9, (index) => Card()),
+            children: List.generate(menus.length, (index) {
+              return Card(
+                color: menus[index].bkColor,
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(20)
+                    )
+                ),
+                child: InkWell(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(menus[index].icon,size: 65,color: Colors.white),
+                      Text(menus[index].title!,style: TextStyle(color: Colors.white))
+                    ],
+                  ),
+                  onTap: () {},
+                ),
+              );
+            })
+        ),
+      ),
+    );
   }
 }
